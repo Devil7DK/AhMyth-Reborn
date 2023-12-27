95f737ad2696d598e3631b38754442d2084ee205
Devil7DK <dineshthangavel47@gmail.com>
2023-12-27 14:33:24 +0530

    Remove MUI & configure Fluent UI

--- AhMyth-Server/src/web/App.tsx
+++ AhMyth-Server/src/web/App.tsx
@@ -1,8 +1,54 @@
 import './App.scss';
 
-import React, { useLayoutEffect } from 'react';
+import {
+    type BrandVariants,
+    createDarkTheme,
+    createLightTheme,
+    FluentProvider,
+    Switch,
+    type Theme,
+} from '@fluentui/react-components';
+import React, { useLayoutEffect, useState } from 'react';
+
+const customTheme: BrandVariants = {
+    10: '#020306',
+    20: '#0F1826',
+    30: '#102744',
+    40: '#0C335C',
+    50: '#033F75',
+    60: '#004C8B',
+    70: '#005AA2',
+    80: '#0067B9',
+    90: '#0075D0',
+    100: '#0083E9',
+    110: '#0F92FF',
+    120: '#559FFF',
+    130: '#78ADFF',
+    140: '#95BCFF',
+    150: '#AECAFF',
+    160: '#C6D9FF',
+};
+
+const lightTheme: Theme = {
+    ...createLightTheme(customTheme),
+};
+
+const darkTheme: Theme = {
+    ...createDarkTheme(customTheme),
+};
+
+darkTheme.colorBrandForeground1 = customTheme[110];
+darkTheme.colorBrandForeground2 = customTheme[120];
 
 export const App: React.FC = () => {
+    const [theme, setTheme] = useState(() =>
+        localStorage.getItem('theme') !== null
+            ? (localStorage.getItem('theme') as 'light' | 'dark')
+            : window.matchMedia('(prefers-color-scheme: dark)').matches
+              ? 'dark'
+              : 'light',
+    );
+
     useLayoutEffect(() => {
         const preloader = document.getElementById('preloader');
 
@@ -15,5 +61,18 @@ export const App: React.FC = () => {
         }
     }, []);
 
-    return <h1>Hello World</h1>;
+    return (
+        <FluentProvider theme={theme === 'dark' ? darkTheme : lightTheme}>
+            <Switch
+                checked={theme === 'dark'}
+                onChange={() => {
+                    setTheme(theme === 'dark' ? 'light' : 'dark');
+                    localStorage.setItem(
+                        'theme',
+                        theme === 'dark' ? 'light' : 'dark',
+                    );
+                }}
+            />
+        </FluentProvider>
+    );
 };
