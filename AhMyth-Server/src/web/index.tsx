6fddc9d9ad3daedcc1e89ff5c34deb0ca7c54182
Devil7DK <dineshthangavel47@gmail.com>
2023-12-29 23:40:17 +0530

    Install and setup leaflet, react-leaflet

--- AhMyth-Server/src/web/index.tsx
+++ AhMyth-Server/src/web/index.tsx
@@ -1,9 +1,21 @@
+import 'leaflet/dist/leaflet.css';
+
+import L from 'leaflet';
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
 import { App } from './App';
 import { StoreProvider } from './store';
+
+delete (L.Icon.Default.prototype as unknown as { _getIconUrl?: () => void })
+    ._getIconUrl;
+
+L.Icon.Default.mergeOptions({
+    iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
+    iconUrl: require('leaflet/dist/images/marker-icon.png'),
+    shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+});
 
 createRoot(document.getElementById('root') as HTMLDivElement).render(
     <StoreProvider>
